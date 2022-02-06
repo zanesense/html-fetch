d0bd99a72d1598d2d2b5907158b1317ff6f15c55
zanez <saimalixd@gmail.com>
2022-02-06 05:03:52 -0800

    Added proxy support

--- html.py
+++ html.py
@@ -1,7 +1,15 @@
-import requests, sys, os
-from huepy import *
+import os,sys,json
 
-os.system('cls')
+try:
+    import requests
+    from huepy import *
+except:
+    print('''\033[91m
+    Required modules are missing, try pip install -r requirements.txt
+    ''')
+    sys.exit()
+
+os.system('clear')
 
 print(green(r"""
  _     _ _______ _______        _______ _______ _______ _______ _     _
@@ -10,18 +18,34 @@ print(green(r"""
                                                                        
 """))
 
-url = input(info('Enter your URL\n> '))
-
+url = input(info("Enter your url\n>> "))
 if url.startswith('https://' or "http://"):
-    print(good('Valid URL!'))
+    print(bad('Don\'t include http or https in the url!!'))
+    sys.exit()
+url_ssl = input(info("Use [1] http or [2] https?\n>> "))
+if url_ssl == "1":
+    finalurl = "http://"+url 
+elif url_ssl == "2":
+    finalurl = "https://"+url
 else:
-    print(bad('Invalid URL provided, exiting...'))
+    print(bad("Provide a valid option!!"))
     sys.exit()
 
-def html_fetch():
-    r = requests.get(url)
-    with open('index.html','w') as f:
-        f.write(r.text)
-        print(good('Successfully wrote the result to index.html'))
+f = open("proxies.json","r")
+proxies = json.load(f)
 
-html_fetch()
+use_proxy = input(info("Use proxy? (y/n)\n>> "))
+def get_html():
+    if use_proxy == "y":
+        r=requests.get(finalurl, proxies=proxies)
+    else:
+        r=requests.get(finalurl)
+    with open("index.html","w") as f:
+        f.write(r.text)
+        print(good("Results exported as index.html in current dir."))
+
+try:
+    get_html()
+except:
+    print(bad("Something happened, check your internet!!"))
+    sys.exit()
